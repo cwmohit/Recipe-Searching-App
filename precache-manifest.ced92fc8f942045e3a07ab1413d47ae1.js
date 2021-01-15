ca902e83034ce44f37d00e05f3138b255965a1b7
Mohit <Mohitkandhari1211krh@gmail.com>
2021-01-15 16:06:48 +0530

    Updates

---
+++ precache-manifest.ced92fc8f942045e3a07ab1413d47ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e19170c678aa89c84c540d1ff6c539",
+    "url": "/Recipe-App-using-react/index.html"
+  },
+  {
+    "revision": "caee5eaf0c0b5cc9b36d",
+    "url": "/Recipe-App-using-react/static/css/main.083fd092.chunk.css"
+  },
+  {
+    "revision": "e0dc3915118cf44f847f",
+    "url": "/Recipe-App-using-react/static/js/2.8dd1790e.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/Recipe-App-using-react/static/js/2.8dd1790e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caee5eaf0c0b5cc9b36d",
+    "url": "/Recipe-App-using-react/static/js/main.60fd7fe8.chunk.js"
+  },
+  {
+    "revision": "4887fb5af5ef489b3d09",
+    "url": "/Recipe-App-using-react/static/js/runtime-main.0a84fd20.js"
+  }
+]);
